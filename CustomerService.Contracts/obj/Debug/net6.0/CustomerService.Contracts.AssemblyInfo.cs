5722a884f489a94504f8189dea0674334a15ad13
Anastasiia Pryesnyakova <anastasiia.presnyakova@wewell.app>
2025-04-24 18:56:08 +0200

    fix build

--- CustomerService.Contracts/obj/Debug/net6.0/CustomerService.Contracts.AssemblyInfo.cs
+++ CustomerService.Contracts/obj/Debug/net6.0/CustomerService.Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomerService.Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a35199903cbeee9137cd636909c36add4931f8de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b768f15d12195090489267f90e3c1c7a9685fd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomerService.Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomerService.Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CustomerService.Infrustructure/obj/Debug/net6.0/CustomerService.Infrustructure.AssemblyInfo.cs
+++ CustomerService.Infrustructure/obj/Debug/net6.0/CustomerService.Infrustructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomerService.Infrustructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a35199903cbeee9137cd636909c36add4931f8de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b768f15d12195090489267f90e3c1c7a9685fd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomerService.Infrustructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomerService.Infrustructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
